ee6c0a29c45090553914577fab36b37cfd47004d
PEDRO ARAUJO <ppteixeira@outlook.com>
2016-04-27 19:08:22 +1000

    add user and stock association and display

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
  
 
+  
   devise_for :users
-  root 'welcome#index'
-  get 'my_portfolio', to: "users#my_portfolio"
-  get 'search_stocks', to: "stocks#search"
+  resources :user_stocks, except: [:show, :edit, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
+  root 'welcome#index'
+  get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
+  
   # root 'welcome#index'
 
   # Example of regular route:
